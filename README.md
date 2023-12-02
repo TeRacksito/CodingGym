1ce7de46c96aa0f915ec09b961ebd7fa8f0fa13c
TeRacksito <acho.chuchi.8@gmail.com>
2023-12-02 17:18:22 +0000

    Add new files and update .gitignore.
    Also fix initialization.

---
+++ README.md
@@ -0,0 +1,49 @@
+# Campus Scrapper
+
+A **daemon Python service** that web scrapes a specific web page, uses IPC connection with a Nextcord Bot.
+
+Configured to not work while IPC connection is unavailable.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+- It's recommended to use Virtual Environment and run `pip install -r requirements.txt`.
+
+- It's needed **geckodriver** on the working directory.
+
+- Also you need two important files.
+
+    - creds.txt
+
+        Contains credentials for the service. In the case of our specific web page, there is three elements, separated by line breaks. Username, Password and IPC secret key.
+    
+    - page_ids.json
+
+        JSON parsable object that has an hash table with this structure:
+        
+        `{key (ID of the page): value (name of the page as string)}`
+
+        This is on the repository.
+
+## Usage
+
+Once you have everything done, you can run bot.py, or you can create an Systemd service to handle the bot (intended way) / (.service files are not included).
+
+If you want to use this simple code to just scrape some page, it's recommended to
+perform some minor changes to remove all IPC code and control flow statements.
+Without that, the code should create a data.json file with all data scraped from the
+web page on it's own.
+
+## Contributing
+
+Not allowed.
+
+## License
+
+You can fork and work with it as you want, no complains.
--- bot.py
+++ bot.py
@@ -1,16 +1,13 @@
 import asyncio
 import json
+import platform
 import sys
 import time
-from math import e
-from pprint import pp, pprint
 from aiohttp.client_exceptions import ClientConnectorError
 
 from selenium import webdriver
-# from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.common.exceptions import NoSuchElementException
-# from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -18,130 +15,135 @@ from nextcord.ext import ipc
 from nextcord.ext.ipc.client import Client
 
 async def main():
-    with open("creds.txt", "r", encoding="utf-8") as f:
-        CREDS = f.read().splitlines()
+    try:
+        with open("creds.txt", "r", encoding="utf-8") as f:
+            CREDS = f.read().splitlines()
 
-    with open("page_ids.json", "r", encoding="utf-8") as f:
-        PAGE_IDS: dict[str, str] = json.load(f)
+        with open("page_ids.json", "r", encoding="utf-8") as f:
+            PAGE_IDS: dict[str, str] = json.load(f)
+    except FileNotFoundError:
+        print("It seems that you don't have the required files. Please, read README.md#installation.")
+        sys.exit(1)
     
     try:
         with open("data.json", "r", encoding="utf-8") as f:
             data: dict[str, list[dict[str, str]]] = json.load(f)
     except FileNotFoundError:
         data = dict()
-        for id in PAGE_IDS.keys():
-            data[id] = list()
+        for page_id in PAGE_IDS.keys():
+            data[page_id] = list()
         update(data)
-    
+
     ipc_client = ipc.Client(secret_key=CREDS[2])
-    
-    # Configurar las opciones de Chrome para el modo headless
-    firefox_options = Options()
-    # firefox_options.add_argument('--headless')  # Ejecutar en modo headless
 
-    # Crear una instancia del navegador
-    driver = webdriver.Firefox(options=firefox_options, executable_path=r'./geckodriver.exe')
-    driver.implicitly_wait(30)
+    options = Options()
+    # options.add_argument('--headless')  # Ejecutar en modo headless
 
-    # Iniciar sesión
+    platform_name = platform.system().lower()
+    match platform_name:
+        case "linux":
+            driver = webdriver.Firefox(options=options, executable_path=r'geckodriver')
+        case "windows":
+            print("Using Windows. This is not recommended.")
+            driver = webdriver.Firefox(options=options, executable_path=r'geckodriver.exe')
+        case _:
+            print("Your OS is not supported, please use Linux.")
+            sys.exit(1)
+
     login(driver, CREDS[0], CREDS[1])
-    
+
     title = driver.title
     print(f'Page: {title}')
 
-    failedTrys = 0
-    while failedTrys < 10:
-        if failedTrys > 0:
+    failed_tries = 0
+    while failed_tries < 10:
+        if failed_tries > 0:
             ipc_client = ipc.Client(secret_key=CREDS[2])
         try:
             ipc_status = await ipc_client.request("status")
             if ipc_status == 200:
-                failedTrys = 0
+                failed_tries = 0
                 await cycle(driver, PAGE_IDS, data, ipc_client)
                 time.sleep(5)
             else:
-                failedTrys += 1
+                failed_tries += 1
                 print("IPC server not ready")
                 time.sleep(5)
-        except ClientConnectorError as e:
-            failedTrys += 1
+        except ClientConnectorError:
+            failed_tries += 1
             print("IPC server not responding, trying again in 10 seconds...")
             time.sleep(10)
 
-    # Cerrar el navegador al finalizar
-    print("It's seems that the IPC server is not responding, closing the program...")
+    print("It's seems that the IPC server is not responding, exiting...")
     driver.quit()
     sys.exit(1)
 
 def login(driver: WebDriver, user: str, password: str) -> None:
     print(f"Attempting to login with {user=:}...")
-    # URL que deseas hacer scraping
     url = 'https://www3.gobiernodecanarias.org/educacion/cau_ce/cas/login?service=https%3A%2F%2Fwww3.gobiernodecanarias.org%2Fmedusa%2Feforma%2Fcampus%2Flogin%2Findex.php'
-    # url = "https://www.google.com"
     driver.get(url)
 
-    userForm = WebDriverWait(driver, 30).until(lambda x: x.find_element(By.ID, "fm1"))
-    userInput = WebDriverWait(driver, 30).until(lambda x: x.find_element(By.ID, "username"))
-    passInput = WebDriverWait(driver, 30).until(lambda x: x.find_element(By.ID, "password"))
+    # userForm = WebDriverWait(driver, 30).until(lambda x: x.find_element(By.ID, "fm1"))
+    user_input = WebDriverWait(driver, 30).until(lambda x: x.find_element(By.ID, "username"))
+    pass_input = WebDriverWait(driver, 30).until(lambda x: x.find_element(By.ID, "password"))
     button = WebDriverWait(driver, 30).until(lambda x: x.find_element(By.ID, "boton-login"))
 
-    userInput.send_keys(user)
-    passInput.send_keys(password)
+    user_input.send_keys(user)
+    pass_input.send_keys(password)
     # userForm.submit()
     button.click()
 
 async def cycle(driver: WebDriver, PAGE_IDS: dict[str, str], data: dict[str, list[dict[str, str]]], ipc_client: Client) -> None:
     print("Starting cycle...")
-    for id, name in PAGE_IDS.items():
-        pageData = data[id]
-        newPage = computeRow(driver, id, PAGE_IDS)
+    for page_id, page_name in PAGE_IDS.items():
+        page_data = data[page_id]
+        new_page = computeRow(driver, page_id, PAGE_IDS)
         time.sleep(1)
 
-        if pageData != newPage:
-            print(f"Page with {id=:} has changed.")
+        if page_data != new_page:
+            print(f"Page with {page_id=:} has changed.")
 
-            # Diferencia entre las dos listas
-            diff = difference(pageData, newPage)
+            diff = difference(page_data, new_page)
 
             if diff:
-                data[id] = newPage
+                data[page_id] = new_page
                 print("There is a difference.")
                 for row in diff:
                     page = scrapePage(driver, row["link"])
                     page["link"] = row["link"]
-                    page["name"] = name
+                    page["name"] = page_name
                     await publish(page, ipc_client)
                     update(data)
                     time.sleep(2)
         time.sleep(5)
-def computeRow(driver: WebDriver, id: str, PAGE_IDS: dict[str, str]) -> list:
-    driver.get(f"https://www3.gobiernodecanarias.org/medusa/eforma/campus/mod/assign/index.php?id={id}")
+def computeRow(driver: WebDriver, page_id: str, PAGE_IDS: dict[str, str]) -> list:
+    driver.get(f"https://www3.gobiernodecanarias.org/medusa/eforma/campus/mod/assign/index.php?id={page_id}")
     title = driver.title
-    print(f'Page: {title}. Id: {id}. Name: {PAGE_IDS[id]}.')
+    print(f'Page: {title}. Id: {page_id}. Name: {PAGE_IDS[page_id]}.')
 
-    mainContent = WebDriverWait(driver, 30).until(lambda x: x.find_element(By.CLASS_NAME, "generaltable"))
-    tbody = mainContent.find_element(By.TAG_NAME, "tbody")
+    main_content = WebDriverWait(driver, 30).until(lambda x: x.find_element(By.CLASS_NAME, "generaltable"))
+    table_body = main_content.find_element(By.TAG_NAME, "tbody")
     rows = list()
-    for row in tbody.find_elements(By.TAG_NAME, "tr"):
+    for row in table_body.find_elements(By.TAG_NAME, "tr"):
         try:
-            rowContent = dict()
+            row_content = dict()
             for cell in row.find_elements(By.TAG_NAME, "td"):
                 colspan = cell.get_attribute("colspan")
                 if colspan:
                     break
                 if cell.get_attribute("class") == "cell c1":
-                    rowContent["title"] = cell.text
-                    rowContent["link"] = cell.find_element(By.TAG_NAME, "a").get_attribute("href")
+                    row_content["title"] = cell.text
+                    row_content["link"] = cell.find_element(By.TAG_NAME, "a").get_attribute("href")
                 elif cell.get_attribute("class") == "cell c2":
                     if cell.text == "-":
-                        rowContent["date"] = "Todavía no hay fecha de entrega."
+                        row_content["date"] = "Todavía no hay fecha de entrega."
                     else:
-                        rowContent["date"] = cell.text
+                        row_content["date"] = cell.text
         except NoSuchElementException as e:
             print(e)
             continue
-        if rowContent:
-            rows.append(rowContent)
+        if row_content:
+            rows.append(row_content)
     return rows
 
 def scrapePage(driver: WebDriver, url: str) -> dict[str, str | list[dict[str, str]]]:
@@ -152,15 +154,15 @@ def scrapePage(driver: WebDriver, url: str) -> dict[str, str | list[dict[str, st
     page = dict()
     urls = list()
 
-    mainContent = WebDriverWait(driver, 30).until(lambda x: x.find_element(By.ID, "region-main"))
-    page["title"] = mainContent.find_element(By.TAG_NAME, "h2").text
-    header = mainContent.find_element(By.CLASS_NAME, "activity-header")
+    main_content = WebDriverWait(driver, 30).until(lambda x: x.find_element(By.ID, "region-main"))
+    page["title"] = main_content.find_element(By.TAG_NAME, "h2").text
+    header = main_content.find_element(By.CLASS_NAME, "activity-header")
     page["content"] = header.text
-    currentUrls = header.find_elements(By.TAG_NAME, "a")
+    current_urls = header.find_elements(By.TAG_NAME, "a")
 
-    for posibleUrl in currentUrls:
-        if posibleUrl.get_attribute("class") != "action-icon":
-            urls.append({"text": posibleUrl.text, "link": posibleUrl.get_attribute("href")})
+    for possible_urls in current_urls:
+        if possible_urls.get_attribute("class") != "action-icon":
+            urls.append({"text": possible_urls.text, "link": possible_urls.get_attribute("href")})
     page["urls"] = urls
     return page
 
@@ -174,9 +176,9 @@ def difference(old: list[dict[str, str]], new: list[dict[str, str]]) -> list:
 
     return diff_list
 
-async def publish(pageData: dict[str, str], ipc_client: Client) -> None:
+async def publish(page_data: dict[str, str], ipc_client: Client) -> None:
     print("Attempting to publish...")
-    await ipc_client.request("publish", pageData=pageData)
+    await ipc_client.request("publish", page_data=page_data)
 
 def update(data: dict[str, list[dict[str, str]]]) -> None:
     with open("data.json", "w", encoding="utf-8") as f:

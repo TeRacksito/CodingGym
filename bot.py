e2d2d9fce3c28cea08986248363e9ac4623de40d
TeRacksito <acho.chuchi.8@gmail.com>
2024-03-04 01:38:14 +0000

    fix

--- bot.py
+++ bot.py
@@ -777,6 +777,10 @@ async def updater(sq: StepQueue, key: str):
             while True:
                 print("Waiting for message...")
                 msg = conn.recv()
+
+                if msg == "status":
+                    conn.send("OK")
+                    continue
                 
                 job = Job(msg['id_exec'], msg["category"], msg['id_user'], msg['path'])
 
